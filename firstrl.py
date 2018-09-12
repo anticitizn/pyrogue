f1a9c679681e72be732a8a8466e98b7e5a89e384
Daniel Neykov <metheorita@gmail.com>
2018-09-13 01:48:42 +0300

    Added working dungeon generation

--- firstrl.py
+++ firstrl.py
@@ -8,6 +8,10 @@ MAP_HEIGHT = 45
 
 LIMIT_FPS = 20
 
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
 color_dark_wall = tcod.Color(0, 0, 100)
 color_dark_ground = tcod.Color(50, 50, 150)
 
@@ -47,6 +51,15 @@ class Rect:
 		self.y1 = y
 		self.x2 = x + w
 		self.y2 = y + h
+	def center(self):
+		center_x = (self.x1 + self.x2) // 2
+		center_y = (self.y1 + self. y2) // 2
+		return (center_x, center_y)
+
+	def intersect(self, other):
+
+		return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+			self.y1 <= other.y2 and self.y2 >= other.y1)
 
 def create_room(room):
 	global map
@@ -76,16 +89,56 @@ def make_map():
 		[Tile(True) for y in range(MAP_HEIGHT)]
 		for x in range(MAP_WIDTH)
 	]
-	room1 = Rect(20, 15, 10, 15)
-	room2 = Rect(50, 15, 10, 15)
-	create_room(room1)
-	create_room(room2)
+	
+	rooms = []
+	num_rooms = 0
 
-	create_h_tunnel(25, 55, 23)
+	for r in range(MAX_ROOMS):
+		w = tcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		h = tcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
 
-	player.x = 25
-	player.y = 23
+		x = tcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+		y = tcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
 
+		new_room = Rect(x, y, w, h)
+
+		failed = False
+		for other_room in rooms:
+			if new_room.intersect(other_room):
+				failed = True
+				break
+		if not failed:
+			#room is valid, no intersections
+
+			create_room(new_room)
+
+			(new_x, new_y) = new_room.center()
+
+			if num_rooms ==0:
+				player.x = new_x
+				player.y = new_y
+		
+			else:
+				#all rooms after the first:
+				#connecting them to the previous with a tunnel
+
+				#center coords of previous room
+				(prev_x, prev_y) = rooms[num_rooms-1].center()
+
+				#get a random bool to decide if vertical or
+				#horizontal tunnel gets created first
+				if tcod.random_get_int(0, 0, 1) == 1:
+					#first horizontal tunnel, then vertical
+					create_h_tunnel(prev_x, new_x, prev_y)
+					create_v_tunnel(prev_y, new_y, new_x)
+				else:
+					#first vertical tunnel, then horizontal
+					create_v_tunnel(prev_y, new_y, prev_x)
+					create_h_tunnel(prev_x, new_x, new_y)
+
+			rooms.append(new_room)
+			num_rooms += 1
+			
 def handle_keys():
 	key = tcod.console_check_for_keypress()
 	if key.vk == tcod.KEY_ENTER and key.lalt:
